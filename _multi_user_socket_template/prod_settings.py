1c14eb6909d29143a191c12e4a212207987c5f41
Jeffrey Kirchner <jkirchner@gmail.com>
2022-09-28 11:16:27 -0700

    add CSRF_TRUSTED_ORIGINS

--- _multi_user_socket_template/prod_settings.py
+++ _multi_user_socket_template/prod_settings.py
@@ -12,6 +12,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = 'main/static/'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split()
 
 # Database
 
