1463354275aff88a1629e00992b2b6eaff9d8ad1
Jeffrey Kirchner <jkirchner@gmail.com>
2022-12-01 11:46:28 -0800

    print instructions dev

--- main/urls.py
+++ main/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('staff-session/<int:pk>/', views.StaffSessionView.as_view(), name='staff_session'),
     path('staff-session/<int:pk>/parameters', views.StaffSessionParametersView.as_view(), name='staff_session_parameters'),
     path('staff-session-subject-earnings/<int:pk>/', views.StaffSessionSubjectEarnings.as_view(), name='staff_session_subject_earnings'),
-
+    path('staff-session-instructions/<int:pk>/', views.StaffSessionInstructions.as_view(), name='staff_session_instructions'),
+    
     path('subject-home/<uuid:player_key>', views.SubjectHomeView.as_view(), name='subject_home'),
 
     path('auto-login/<uuid:session_key>/', views.SubjectHomeAutoConnectView.as_view(), name='subject_home_auto_connect'),
--- main/views/staff/__init__.py
+++ main/views/staff/__init__.py
@@ -5,5 +5,6 @@ from .staff_home import StaffHomeView
 from .staff_session import StaffSessionView
 from .staff_session_parameters import StaffSessionParametersView
 from .staff_session_subject_earnings import StaffSessionSubjectEarnings
+from .staff_session_instructions import StaffSessionInstructions
 
 
