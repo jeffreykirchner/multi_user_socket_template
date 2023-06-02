a0d37235b6747c31e14c31a3f77852eb2ea67dd3
Jeffrey Kirchner <jkirchner@gmail.com>
2023-06-02 16:07:21 -0700

    display earnings on staff screen. fixes #91.

--- main/consumers/staff/session_consumer_mixins/subject_updates.py
+++ main/consumers/staff/session_consumer_mixins/subject_updates.py
@@ -147,8 +147,8 @@ class SubjectUpdatesMixin():
         if self.controlling_channel != self.channel_name:
             return
         
-        logger = logging.getLogger(__name__) 
-        logger.info(f"target_location_update: world state controller {self.controlling_channel} channel name {self.channel_name}")
+        # logger = logging.getLogger(__name__) 
+        # logger.info(f"target_location_update: world state controller {self.controlling_channel} channel name {self.channel_name}")
         
         logger = logging.getLogger(__name__)
         
