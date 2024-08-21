5572a7d0d6e450f45b5817d9f93317d562db991d
Jeffrey Kirchner <jkirchner@gmail.com>
2024-08-21 18:28:13 +0000

    Roll back replay cache feature. fixes #240.

--- main/consumers/staff/session_consumer_mixins/interface.py
+++ main/consumers/staff/session_consumer_mixins/interface.py
@@ -17,27 +17,27 @@ class InterfaceMixin():
         session = await Session.objects.aget(id=self.session_id)
         session_events_local = {}
 
-        if session.replay_data:
-            session_events_local = session.replay_data
-        else:
-            async for i in session.session_periods.all():
+        # if session.replay_data:
+        #     session_events_local = session.replay_data
+        # else:
+        async for i in session.session_periods.all():
 
-                session_events_local[str(i.period_number)] = {}
+            session_events_local[str(i.period_number)] = {}
 
-                total_period_length = self.parameter_set_local["period_length"]
+            total_period_length = self.parameter_set_local["period_length"]
 
-                if i.period_number % self.parameter_set_local["break_frequency"] == 0:
-                    total_period_length += self.parameter_set_local["break_length"]
+            if i.period_number % self.parameter_set_local["break_frequency"] == 0:
+                total_period_length += self.parameter_set_local["break_length"]
 
-                for j in range(total_period_length+1):
-                    session_events_local[str(i.period_number)][str(j)] = []
+            for j in range(total_period_length+1):
+                session_events_local[str(i.period_number)][str(j)] = []
 
-            async for i in session.session_events.exclude(type="help_doc"):
-                v = {"type" : i.type, "data" : i.data}
-                session_events_local[str(i.period_number)][str(i.time_remaining)].append(v)
+        async for i in session.session_events.exclude(type="help_doc"):
+            v = {"type" : i.type, "data" : i.data}
+            session_events_local[str(i.period_number)][str(i.time_remaining)].append(v)
 
-            session.replay_data = session_events_local
-            await session.asave()
+        # session.replay_data = session_events_local
+        # await session.asave()
 
         result = {"session_events": session_events_local}
 
