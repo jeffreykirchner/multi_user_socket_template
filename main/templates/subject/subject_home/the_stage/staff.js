4634beb914e0cb19ff2a8be2cb24ea16589c96e6
Jeffrey Kirchner <jkirchner@gmail.com>
2024-08-27 10:38:11 -0700

    Update staff screen screen scroll buttons. fixes #205.

--- main/templates/subject/subject_home/the_stage/staff.js
+++ main/templates/subject/subject_home/the_stage/staff.js
@@ -9,8 +9,9 @@ add_scroll_button: function add_scroll_button(button_size, name, text)
     
     g.rect(0, 0, button_size.w, button_size.h);
 
-    g.stroke(1, 0x000000);
+    
     g.fill({color:0xffffff});
+    g.stroke(1, 0x000000);
 
     let label = new PIXI.Text({text:text, 
                                style:{fontFamily : 'Arial',
@@ -29,6 +30,7 @@ add_scroll_button: function add_scroll_button(button_size, name, text)
     c.pivot.set(button_size.w/2, button_size.h/2);
     c.x = button_size.x;
     c.y = button_size.y;
+    c.eventMode = 'static';
     c.label = name;
     c.alpha = 0.5;
 
