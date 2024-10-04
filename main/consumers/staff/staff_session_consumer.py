2d6bfe76f00629df02f569205880c331509ba60f
Jeffrey Kirchner <jkirchner@gmail.com>
2024-10-04 18:18:35 +0000

    Refactor StaffSessionConsumer class

--- main/consumers/staff/staff_session_consumer.py
+++ main/consumers/staff/staff_session_consumer.py
@@ -20,8 +20,7 @@ class StaffSessionConsumer(SocketConsumerMixin,
                            InterfaceMixin,
                            WorldStateMixin,
                            SendMessageMixin):
-
-    has_timer_control = False         #this instance is controlling the timer           
+        
     world_state_local = {}            #local copy of world state
     session_players_local = {}        #local copy of session players
     parameter_set_local = {}          #local copy of parameter set   
