b24c9e6e5661c892128f30fc3677081ce87807c3
Jeffrey Kirchner <jkirchner@gmail.com>
2024-12-03 17:33:17 +0000

    Remove overcommit_memory setting from startup script for Redis configuration

--- startup.sh
+++ startup.sh
@@ -6,6 +6,5 @@ echo "Install htop:"
 apt-get -y install htop
 echo "Install redis"
 apt-get -y install redis
-sysctl vm.overcommit_memory=1
 echo "Start Server:"
 redis-server & daphne -b 0.0.0.0 _multi_user_socket_template.asgi:application
