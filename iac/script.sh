ae28e157b62595c45aae391c404ded2645d56601
Jeffrey Kirchner <jkirchner@gmail.com>
2024-12-11 23:42:17 +0000

    iac dev

--- iac/script.sh
+++ iac/script.sh
@@ -2,7 +2,10 @@
 
 webapp_name="chapman-experiments-template"
 resource_group="RG_ESI_VMs"
+resouce_group_db="Experiments"
 app_service_plan='chapman-esi-asp'
+storage_account='chapmanexperimentsstor'
+postgres_server='chapman-experiments-db-v13'
 
 echo "Starting deployment ..."
 
@@ -24,10 +27,16 @@ az webapp config storage-account add \
   --name $webapp_name \
   --custom-id logs \
   --storage-type AzureFiles \
-  --account-name chapmanexperimentsstor \
+  --account-name $storage_account \
   --share-name logs \
   --access-key $access_key \
   --mount-path /logs
+
+az storage directory create \
+    --name $webapp_name \
+    --share-name logs \
+    --account-name $storage_account \
+    --account-key $access_key
 
 az webapp log config \
     --name $webapp_name \
@@ -37,3 +46,21 @@ az webapp log config \
 az webapp identity assign \
     --name $webapp_name \
     --resource-group $resource_group
+
+az webapp config appsettings set \
+    --name $webapp_name \
+    --resource-group $resource_group \
+    --settings "@environment_variables.json"
+
+echo "Enter database admin username:"
+read admin_user
+
+echo "Enter database admin password:"
+read admin_password
+
+az postgres flexible-server execute --admin-user $admin_user \
+                                    --admin-password $admin_password \
+                                    --name $postgres_server \
+                                    --output table \
+                                    --querytext "SELECT datname FROM pg_database;"
+
