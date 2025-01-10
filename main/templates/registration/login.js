6d7cbff23f1b70d49b740780e1eb3aec7ce1e9d8
Jeffrey Kirchner <jkirchner@gmail.com>
2025-01-10 19:52:35 +0000

    feat: implement two-factor authentication in login process with QR code setup. fixes #242.

--- main/templates/registration/login.js
+++ main/templates/registration/login.js
@@ -13,6 +13,12 @@ let app = Vue.createApp({
         form_ids : {{form_ids|safe}},
         username : null,
         password : null,
+        show_two_factor : false,
+        two_factor_code : "",  
+        show_two_factor_setup : false,    
+        two_factor_uri : "",       
+        two_factor_hash : "",   
+        qr_code : null,
         }                          
     },
 
@@ -26,29 +32,57 @@ let app = Vue.createApp({
 
             axios.post('/accounts/login/', {
                     action :"login",
-                    form_data : {username:app.username, password:app.password},                              
+                    form_data : {username:app.username, password:app.password}, 
+                    two_factor_code : app.two_factor_code,                             
                 })
                 .then(function (response) {     
                     
-                status=response.data.status;                               
+                    status=response.data.status;                               
 
-                app.clear_main_form_errors();
+                    app.clear_main_form_errors();
 
-                if(status == "validation")
-                {              
-                    //form validation error           
-                    app.display_errors(response.data.errors);
-                }
-                else if(status == "error")
-                {
-                    app.login_error_text = response.data.message;
-                }
-                else
-                {
-                    window.location = response.data.redirect_path;
-                }
+                    if(status == "validation")
+                    {              
+                        //form validation error           
+                        app.display_errors(response.data.errors);
+                    }
+                    else if(status == "error")
+                    {
+                        app.login_error_text = response.data.message;
+                    }
+                    else if(status == "two_factor")
+                    {
+                        app.show_two_factor = true;
+        
+                        Vue.nextTick(() => {
+                            document.getElementById("idtwofactorcode").focus();
+                        });
+                        
+                    }
+                    else if(status == "two_factor_setup")
+                    {
+                        app.show_two_factor = true;
+                        app.show_two_factor_setup = true;
+                        app.two_factor_uri = response.data.two_factor_uri;
+                        app.two_factor_hash = response.data.two_factor_hash;
+        
+                        if(app.qr_code != null)
+                        {
+                            app.qr_code.clear();
+                            app.qr_code.makeCode(app.two_factor_uri);
+                        }
+                        else
+                        {
+                            app.qr_code = new QRCode(document.getElementById("qrcode"), app.two_factor_uri);
+                        }
+                        
+                    }
+                    else
+                    {
+                        window.location = response.data.redirect_path;
+                    }
 
-                app.login_button_text = 'Submit <i class="fas fa-sign-in-alt"></i>';
+                    app.login_button_text = 'Submit <i class="fas fa-sign-in-alt"></i>';
 
                 })
                 .catch(function (error) {
