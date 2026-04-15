789462071dc86a06acd4c608c54dfe14a989f21d
Jeffrey Kirchner <jkirchner@gmail.com>
2026-04-15 13:49:28 -0700

    Fix: Update database names in setup script for consistency

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@ echo "Setup multi user socket template"
 sudo service postgresql restart
 sudo service redis-server start
 echo "Drop template db: enter db password"
-dropdb multi_user_socket_template -U dbadmin -h localhost -i -p 5432
+dropdb template_experiment -U dbadmin -h localhost -i -p 5432
 echo "Create database: enter db password"
-createdb -h localhost -p 5432 -U dbadmin -O dbadmin multi_user_socket_template
+createdb -h localhost -p 5432 -U dbadmin -O dbadmin template_experiment
 echo "Restore database? (y/n)"
 read restore
 if [ "$restore" = "y" ]; then
     echo "Restore database: enter db password"
-    pg_restore -v --no-owner --role=dbowner --host=localhost --port=5432 --username=dbadmin --dbname=multi_user_socket_template database_dumps/multi_user_socket_template.sql
+    pg_restore -v --no-owner --role=dbowner --host=localhost --port=5432 --username=dbadmin --dbname=template_experiment database_dumps/template_experiment.sql
 else
     python manage.py migrate
     echo "Create Super User:"
